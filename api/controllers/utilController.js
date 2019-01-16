b4bcaddcddebd67bb6087c1e48d0c5984a8d5a54
Jorge Javier Dacev <jdacev@grupoassa.com>
2019-01-16 16:55:01 -0300

    generateSecurityCode

--- api/controllers/utilController.js
+++ api/controllers/utilController.js
@@ -4,8 +4,12 @@ var mailHelper = require('./helpers/mailHelper');
 var mailTemplateHelper = require('./helpers/mailTemplateHelper');
 
 
+function generateSecurityCode () {
+    return Math.floor(Math.random() * (1000000));
+  }
+
 var sendSecurityCode = function(req, res) {
-    const mailTemplate = mailTemplateHelper.securityCode(123456);
+    const mailTemplate = mailTemplateHelper.securityCode(generateSecurityCode());
     mailHelper.send(req.body.email,
         mailTemplate.subject, 
         mailTemplate.html, mailTemplate.text);
